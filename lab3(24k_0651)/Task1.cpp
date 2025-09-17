eaaec4568b49a61ee2f3181ad98555a25f93b6b8
TEAROFJOY5 <turab653@gmail.com>
2025-09-17 06:10:02 -0700

    Update Task1.cpp

--- lab3(24k_0651)/Task1.cpp
+++ lab3(24k_0651)/Task1.cpp
@@ -1,1 +1,17 @@
 
+#include <iostream>
+using namespace std;
+class Student {
+public:
+    string name;
+    int rollNumber;
+};
+int main() {
+    Student s;
+    s.name = "Turab Ali";
+    s.rollNumber = 123;
+    cout << "Name: " << s.name << endl;
+    cout << "Roll Number: " << s.rollNumber << endl;
+    return 0;
+}
+
